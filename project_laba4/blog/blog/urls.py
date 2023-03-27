aff86c49e61dba486044e13b93b7d4c1d347511f
mango-324 <mashamango324@gmail.com>
2023-03-28 02:29:46 +0300

    лаба 4

---
+++ project_laba4/blog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from articles import views
+from django.urls import path, re_path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('archive/', views.archive),
+    re_path(r'^article/(?P<article_id>\d+)$', views.get_article, name='get_article'),
+]
+
+
